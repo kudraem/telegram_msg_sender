0f87e988623a6da86dea787469400e41a1dce37e
Nikita Savin <klaster26@mail.ru>
2024-02-02 18:16:43 +0300

    feat: реализовать метод getupdates для TgBotApi

--- tg_bot_api/tests/test_tg_bot_api.py
+++ tg_bot_api/tests/test_tg_bot_api.py
@@ -1,3 +1,61 @@
 import pytest
-from tg_bot_api.tg_bot_api import tg_bot_api, TgBotApiException
+from tg_bot_api.tg_bot_api import TgBotApi, TgBotApiException
 
+
+URL = 'https://api.telegram.org/bot'
+TOKEN = '6929213342:AAGQHDN--h2bg7k2IT-s019317WdmICQ0D4'
+
+
+def test_tg_bot_api():
+    new = TgBotApi(URL, TOKEN)
+    response = new.get_updates()
+    assert response['ok'] is True
+    print('Tests for Telegram Bot API passed')
+
+
+def test_raise_http_exception():
+    with pytest.raises(TgBotApiException) as err:
+        new = TgBotApi(URL, 'hello-world')
+        new.get_updates()
+    assert str(err.value) == ('HTTPError is occured, '
+                              'and it is 404 Client Error: '
+                              'Not Found for url:'
+                              ' https://api.telegram.org/bothello-world/getUpdates')
+    print('Test passed, HTTP error is occured')
+
+
+'''
+def test_max_redirects():
+    with pytest.raises(DummyJsonException) as err:
+        new = DummyJsonApi('https://2866-79-101-225-134.ngrok-free.app/redirect_error')
+        new.get()
+    assert str(err.value) == 'Sorry, too many redirects'
+
+
+def test_timeout():
+    with pytest.raises(DummyJsonException) as err:
+        new = DummyJsonApi('https://2866-79-101-225-134.ngrok-free.app/timeout_error')
+        new.get()
+    assert str(err.value) == 'Timeout error. Try again later.'
+'''
+
+
+# К сожалению, фактически в тесте нет смысла
+# Нет доступного ресурса, который бы по ссылке
+# {...}/getUpdates выдавал бы не 404, а просто
+# невалидный JSON
+'''
+def test_json_parse():
+    with pytest.raises(TgBotApiException) as err:
+        new = TgBotApi('https://validator.w3.org/images/w3c.png', token='')
+        new.get_updates()
+    print(err.value)
+'''
+
+
+def test_connection():
+    with pytest.raises(TgBotApiException) as err:
+        new = TgBotApi('https://gooogle.com/404', TOKEN)
+        new.get_updates()
+    assert str(err.value) == 'Connection is lost, try again later.'
+    print('Tests passed. Connection exception is caught')
--- tg_bot_api/tg_bot_api/tg_bot_api.py
+++ tg_bot_api/tg_bot_api/tg_bot_api.py
@@ -1,5 +1,8 @@
 import requests
 
+URL = 'https://api.telegram.org/bot'
+TOKEN = '6929213342:AAGQHDN--h2bg7k2IT-s019317WdmICQ0D4'
+
 
 class TgBotApiException(Exception):
     def __init__(self, message):
@@ -10,19 +13,34 @@ class TgBotApiException(Exception):
 
 
 class TgBotApi(requests.Session):
-    def __init__(self):
+    def __init__(self, url, token):
         super().__init__()
+        self.url = url
+        self.token = token
         self.max_redirects = 5
         self.headers = {'User-Agent': 'Python-Study-App/1.0.0',
                         'Content-Type': 'application/json'}
         self.timeout = 5.0
 
+    def make_request(self, method, path, **kwargs):
+        try:
+            response = self.request(method, path, **kwargs)
+            response.raise_for_status()
+            return response.json()
+        except requests.JSONDecodeError:
+            raise TgBotApiException('Incoming JSON is invalid')
+        except requests.TooManyRedirects:
+            raise TgBotApiException('Sorry, too many redirects')
+        except requests.HTTPError as err:
+            raise TgBotApiException(f'HTTPError is occured, and it is {err}')
+        except requests.Timeout:
+            raise TgBotApiException('Timeout error. Try again later.')
+        except requests.ConnectionError:
+            raise TgBotApiException('Connection is lost, try again later.')
+
     def get_bot_username(self):
         return "Telegram Bot API"
 
-    def get_bot_token(self):
-        return '6929213342:AAGQHDN--h2bg7k2IT-s019317WdmICQ0D4'
-
-    def on_update_received(self, update):
-        return update
-
+    def get_updates(self):
+        url = f'{self.url}{self.token}/getUpdates'
+        return self.make_request('get', url)
